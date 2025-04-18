cb59d8d9abf15c721c8ec3a211d81e8c1b3ffa2d
QianyuIsSally <luq21@wfu.edu>
2025-04-17 20:53:21 -0400

    Add files via upload

---
+++ run.R
@@ -0,0 +1,5 @@
+library(shiny)
+path_to_app = runApp("~/ACSNavigateR/")
+#path_to_app = "~/Dropbox (Wake Forest University)/8 - STUDENTS/Sally-ACS-NavigatR/ACSNavigatR/"
+setwd(path_to_app)
+runApp(path_to_app)
